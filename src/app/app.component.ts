262f9f63c646c4d651804b2774aec523a2119a97
MBARGAM <77581549+MBARGAM@users.noreply.github.com>
2026-03-05 16:34:23 +0100

    update
    mise a jour du site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
-import { ServicesComponent } from './components/services/services.component';
-import { WhyUsComponent } from './components/why-us/why-us.component';
+import { BodyComponent } from './components/body/body.component';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +11,8 @@ import { WhyUsComponent } from './components/why-us/why-us.component';
   imports: [RouterOutlet,
     HeaderComponent,
     SliderComponent,
-    ServicesComponent,
-    WhyUsComponent,
-    ContactComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
