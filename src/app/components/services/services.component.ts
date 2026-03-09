394b999ae27c9c82b3b434532d5f17db88538a55
MBARGAM <77581549+MBARGAM@users.noreply.github.com>
2026-03-09 12:57:51 +0100

    upd
    mise a jour

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
-import {CommonModule } from '@angular/common';
+import { Component ,ChangeDetectorRef} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [CommonModule,TranslatePipe ],
+  imports: [CommonModule,TranslatePipe],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss',
 })
 export class ServicesComponent {
-  constructor(private translate : TranslateService){}
-   
+ isFrench = true ;
+ constructor(private translate: TranslateService, private cdr: ChangeDetectorRef) {}
    // fonction de traduction 
-  translateText(lang : string ){
-    this.translate.use(lang);
-    localStorage.setItem('lang', lang); // Sauvegarde la langue sélectionnée
-  }
+translateText(lang: string) {
+  this.translate.use(lang);
+  localStorage.setItem('lang', lang);
+  // La bonne logique :
+  this.isFrench = lang === 'fr';
+  this.cdr.detectChanges();
+}
+
+
 }
